9f6ed85c539671471e3dbaba62d0beb66f5ad816
timflannagan <timflannagan@gmail.com>
2025-01-23 17:25:42 +0000

    *: Update constant variables, static testdata, etc. to reflect new GatewayClass name
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- projects/gateway2/deployer/deployer_test.go
+++ projects/gateway2/deployer/deployer_test.go
@@ -1406,15 +1406,12 @@ var _ = Describe("Deployer", func() {
 				if sdsContainer.SecurityContext != nil {
 					Expect(sdsContainer.SecurityContext.RunAsUser).To(BeNil())
 				}
-
 				if gwContainer.SecurityContext != nil {
 					Expect(gwContainer.SecurityContext.RunAsUser).To(BeNil())
 				}
-
 				if istioProxyContainer.SecurityContext != nil {
 					Expect(istioProxyContainer.SecurityContext.RunAsUser).To(BeNil())
 				}
-
 				if aiContainer.SecurityContext != nil {
 					Expect(aiContainer.SecurityContext.RunAsUser).To(BeNil())
 				}
@@ -1557,7 +1554,7 @@ var _ = Describe("Deployer", func() {
 						APIVersion: "gateway.solo.io/v1beta1",
 					},
 					Spec: api.GatewaySpec{
-						GatewayClassName: "gloo-gateway",
+						GatewayClassName: wellknown.GatewayClassName,
 					},
 				},
 				defaultGwp: defaultGatewayParams(),
@@ -1591,7 +1588,7 @@ var _ = Describe("Deployer", func() {
 						APIVersion: "gateway.solo.io/v1beta1",
 					},
 					Spec: api.GatewaySpec{
-						GatewayClassName: "gloo-gateway",
+						GatewayClassName: wellknown.GatewayClassName,
 						Listeners: []api.Listener{
 							{
 								Name: "listener-1",
